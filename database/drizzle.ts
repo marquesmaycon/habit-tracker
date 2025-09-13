00aa4c9439812d2a443c3de68e456d20ebab643d
Maycon Marques <mayconmarquesh@gmail.com>
2025-09-13 12:42:15 -0300

    drizzle config

---
+++ database/drizzle.ts
@@ -0,0 +1,3 @@
+import { drizzle } from "drizzle-orm/better-sqlite3"
+
+export const db = drizzle(process.env.DB_FILE_NAME || "")
---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit"
+
+export default defineConfig({
+  schema: "./database/schemas.ts",
+  out: "./database/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME || ""
+  }
+})
