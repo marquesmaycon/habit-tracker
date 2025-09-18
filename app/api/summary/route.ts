d56498fd7190db41c5669b86ef1dd4bded0e1f8d
Maycon Marques <mayconmarquesh@gmail.com>
2025-09-18 12:31:49 -0300

    summary route fixed

--- app/api/summary/route.ts
+++ app/api/summary/route.ts
@@ -1,14 +1,38 @@
-import { eq } from "drizzle-orm";
+import { sql } from "drizzle-orm"
 import { NextResponse } from "next/server";
-import { db } from "@/database/drizzle";
-import { habitDays, habits, habitWeekdays } from "@/database/schemas";
+import { db } from "@/database/drizzle"
 
 export async function GET() {
-  const Allhabits = db
-    .select()
-    .from(habits)
-    .leftJoin(habitWeekdays, eq(habits.id, habitWeekdays.habitId))
-    .leftJoin(habitDays, eq(habits.id, habitDays.habitId))
-    .all();
-  return NextResponse.json({ data: Allhabits });
+  try {
+    const summary = db.all(sql`
+      SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `)
+
+    return NextResponse.json(summary)
+  } catch (error) {
+    console.error("Erro ao gerar summary:", error)
+    return NextResponse.json(
+      { message: "Erro interno do servidor" },
+      { status: 500 },
+    )
+  }
 }
